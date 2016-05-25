c76dd0607bc6bd0350dc4e7289575af9d0e6e6ab
grit0614 <grit0614@gmail.com>
2016-05-25 08:58:14 +0000

    jQuery Animation Implementation

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
   post 'home/write'
-  get '/summary' => 'home#summary'
+  # get '/summary' => 'home#summary'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
